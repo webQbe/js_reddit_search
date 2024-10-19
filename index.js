6fef346812426f01389dc0bd8ee163759eba4aa9
webQbe <webcubesolutionslk@gmail.com>
2024-10-19 16:16:56 +0530

    Added post text to cards

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ searchForm.addEventListener('submit', e => {
                     <img class="card-img-top" src="..." alt="Card image cap">
                     <div class="card-body">
                         <h5 class="card-title">${post.title}</h5>
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <p class="card-text">${post.selftext}</p>
                         <a href="#" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>`;
